9b2d329003e0cbf358f81fa97b343d869801d193
Andrew Bingham <andrewgbingham@gmail.com>
2023-06-12 10:45:09 -0600

    feat: finish serializer

--- app/serializers/books_serializer.rb
+++ app/serializers/books_serializer.rb
@@ -8,14 +8,23 @@ class BooksSerializer
   attributes :forecast do |object|
     {
       summary: object.forecast_data.data[:current][:condition][:text],
-      temperature: object.forecast_data.data[:current][:temp_f]
+      temperature: object.forecast_data.data[:current][:temp_f].to_s + " F"
     }
   end
 
+  attributes :total_books_found do |object|
+    object.book_data[:numFound]
+  end
 
-
-
-
+  attributes :books do |object|
+    object.book_data[:docs].map do |book|
+      {
+        isbn: book[:isbn],
+        title: book[:title],
+        publisher: book[:publisher]
+      }
+    end
+  end
 end
 
 
--- spec/serializers/books_serializer_spec.rb
+++ spec/serializers/books_serializer_spec.rb
@@ -16,16 +16,23 @@ RSpec.describe BooksSerializer do
     expect(json[:data]).to be_a(Hash)
     expect(json[:data][:id]).to eq(nil)
     expect(json[:data][:type]).to eq('books')
-    
+
     expect(json[:data][:attributes]).to be_a(Hash)
     expect(json[:data][:attributes][:destination]).to be_a(String)
     expect(json[:data][:attributes][:destination]).to eq('denver,co')
 
     expect(json[:data][:attributes][:forecast]).to be_a(Hash)
     expect(json[:data][:attributes][:forecast][:summary]).to be_a(String)
+    expect(json[:data][:attributes][:forecast][:temperature]).to be_a(String)
 
-    
+    expect(json[:data][:attributes][:total_books_found]).to be_a(Integer)
 
+    expect(json[:data][:attributes][:books]).to be_an(Array)
+    expect(json[:data][:attributes][:books].count).to eq(5)
 
+    expect(json[:data][:attributes][:books][0]).to be_a(Hash)
+    expect(json[:data][:attributes][:books][0][:isbn]).to be_a(Array)
+    expect(json[:data][:attributes][:books][0][:title]).to be_a(String)
+    expect(json[:data][:attributes][:books][0][:publisher]).to be_a(Array)
   end
 end
