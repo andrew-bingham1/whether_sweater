a23135df353fbbc7418aadca9b0d9a86c635e98d
Andrew Bingham <andrewgbingham@gmail.com>
2023-06-13 15:05:16 -0600

    test/fix: create timezone service test, flesh out existing tests

--- spec/services/forecast_service_spec.rb
+++ spec/services/forecast_service_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe ForecastService do
-  it 'can get a forecast', :vcr do
+  it 'can get a forecast',  vcr: { record: :new_episodes } do
     forecast_service = ForecastService.new
     lat = 39.74001
     lng = -104.99202
@@ -26,5 +26,37 @@ RSpec.describe ForecastService do
     expect(forecast_response[:forecast]).to have_key(:forecastday)
     expect(forecast_response[:forecast][:forecastday]).to be_an(Array) 
     expect(forecast_response[:forecast][:forecastday].count).to eq(6)
+  end
+
+  it 'can get a forecast for a location at a specific time',  vcr: { record: :new_episodes } do
+    forecast_service = ForecastService.new
+    city = 'denver'
+    date = '2023-06-25'
+    hour = '16'
+
+    forecast_response = forecast_service.get_future_forecast(city, date, hour)
+    
+    expect(forecast_response).to be_a(Hash)
+
+    expect(forecast_response).to have_key(:forecast)
+    expect(forecast_response[:forecast]).to be_a(Hash)
+
+    expect(forecast_response[:forecast]).to have_key(:forecastday)
+    expect(forecast_response[:forecast][:forecastday]).to be_an(Array)
+
+    expect(forecast_response[:forecast][:forecastday][0]).to be_a(Hash)
+    expect(forecast_response[:forecast][:forecastday][0]).to have_key(:hour)
+    expect(forecast_response[:forecast][:forecastday][0][:hour]).to be_an(Array)
+    expect(forecast_response[:forecast][:forecastday][0][:hour][0]).to be_a(Hash)
+
+    expect(forecast_response[:forecast][:forecastday][0][:hour][0]).to have_key(:time)
+    expect(forecast_response[:forecast][:forecastday][0][:hour][0][:time]).to be_a(String)
+
+    expect(forecast_response[:forecast][:forecastday][0][:hour][0]).to have_key(:temp_f)
+    expect(forecast_response[:forecast][:forecastday][0][:hour][0][:temp_f]).to be_a(Float)
+
+    expect(forecast_response[:forecast][:forecastday][0][:hour][0][:condition]).to be_a(Hash)
+    expect(forecast_response[:forecast][:forecastday][0][:hour][0][:condition]).to have_key(:text)
+    expect(forecast_response[:forecast][:forecastday][0][:hour][0][:condition][:text]).to be_a(String)
   end
 end
