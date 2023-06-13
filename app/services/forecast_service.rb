c3c4496d161e4922e3ffbc18817a195d9f905362
Andrew Bingham <andrewgbingham@gmail.com>
2023-06-13 15:32:59 -0600

    feat: create timezone service

--- app/services/forecast_service.rb
+++ app/services/forecast_service.rb
@@ -5,6 +5,10 @@ class ForecastService
   def get_forecast(lat, lng)
     get_url("v1/forecast.json?key=#{ENV['WEATHER_API_KEY']}&q=#{lat},#{lng}&days=6&aqi=no&alerts=no")
   end
+
+  def get_future_forecast(city, date, hour)
+    get_url("v1/forecast.json?key=#{ENV['WEATHER_API_KEY']}&q=#{city}&dt=#{date}&hour=#{hour}&aqi=no&alerts=no")
+  end
   
   private
 
@@ -16,5 +20,5 @@ class ForecastService
     response = conn.get(url)
     JSON.parse(response.body, symbolize_names: true)
   end
-  
+
 end
--- app/services/location_service.rb
+++ app/services/location_service.rb
@@ -1,6 +1,17 @@
 require 'faraday'
 
 class LocationService
+  
+  def get_location(location)
+    get_url("/geocoding/v1/address?key=#{ENV['MAPQUEST_API_KEY']}&location=#{location}")
+  end
+
+  def get_directions(start, destination)
+    get_url("/directions/v2/route?key=#{ENV['MAPQUEST_API_KEY']}&from=#{start}&to=#{destination}")
+  end
+
+  private
+
   def conn
     Faraday.new(url: "https://www.mapquestapi.com")
   end
@@ -8,9 +19,5 @@ class LocationService
   def get_url(url)
     response = conn.get(url)
     JSON.parse(response.body, symbolize_names: true)
-  end
-
-  def get_location(location)
-    get_url("/geocoding/v1/address?key=#{ENV['MAPQUEST_API_KEY']}&location=#{location}")
   end
 end
