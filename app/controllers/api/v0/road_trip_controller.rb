81673fa5659dae95a92314ed4bad2364b605935d
Andrew Bingham <andrewgbingham@gmail.com>
2023-06-13 15:37:11 -0600

    fix: add strong params to roadtrip

--- app/controllers/api/v0/road_trip_controller.rb
+++ app/controllers/api/v0/road_trip_controller.rb
@@ -1,4 +1,5 @@
 class Api::V0::RoadTripController < ApplicationController
+  wrap_parameters :road_trip, include: [:origin, :destination, :api_key]
   def create
     user = User.find_by(api_key: params[:api_key])
     if user 
@@ -7,5 +8,9 @@ class Api::V0::RoadTripController < ApplicationController
     else
       render json: { errors: 'Bad Credentials' }, status: :unauthorized
     end
+  end
+
+  def road_trip_params
+    params.require(:road_trip).permit(:origin, :destination, :api_key)
   end
 end
